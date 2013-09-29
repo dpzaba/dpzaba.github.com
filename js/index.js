b052131164a872b7ef8a7cb9a46a530c5612e768
dpzaba <dpzaba@gmail.com>
2013-09-29 13:40:11 +0200

    Added fontawesome
    Separated Js
    
    Beginning refactoring

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function my_photo(){
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    var img=new Image();
+    img.onload = function(){
+        ctx.drawImage(img,0,0);
+    };
+    var part1="img/w";
+    var part2="eb.p";
+    var part3="ng";
+    img.src=part1+part2+part3;
+}
+
+function my_email(){
+    var email1="dpzaba"
+    var email2="@"
+    var email3="gmail."+"com"
+    var contact=document.getElementById("contact");
+    var fullEmail = email1+email2+email3;
+
+    contact.href="mailto:" + fullEmail;
+    contact.innerHTML="Email: " + fullEmail;
+}
+
+function start(){
+    my_email();
+    my_photo();
+}
+
+window.onload=start;
